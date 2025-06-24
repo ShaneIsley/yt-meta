6f628a9c98c605d68d36b17ebae8560a325dc02c
Shane <shane.isley@gmail.com>
2025-06-24 20:07:40 +1000

    chore: Bump version to 0.2.2 for release

--- yt_meta/__init__.py
+++ yt_meta/__init__.py
@@ -4,7 +4,7 @@ from .client import YtMetaClient
 from .date_utils import parse_relative_date_string
 from .exceptions import MetadataParsingError, VideoUnavailableError
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 __all__ = [
     "YtMetaClient",
